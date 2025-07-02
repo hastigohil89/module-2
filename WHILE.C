94686d28de4883f77556d5ed5a874838ce2b19dd
hastigohil89 <hastigohil89@gmail.com>
2025-07-01 21:30:59 -0700

    Add files via upload

---
+++ WHILE.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   int n;
+   clrscr();
+   printf("\n enter N: ");
+   scanf("%d",&n);
+   while(n>0)
+   {
+      printf("\n tops technologies");
+      n--;
+   }
+   getch();
+}
