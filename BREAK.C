a1b9d3292bba89d781bbd27f4c9cf678d8a4e6d8
hastigohil89 <hastigohil89@gmail.com>
2025-07-04 22:42:42 -0700

    Add files via upload

---
+++ BREAK.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int i;
+ clrscr();
+  for(i=0;i<10;i++)
+  {
+     if(i==5)
+     {
+	 continue;
+     }
+     else
+     {
+       printf("\ni: %d",i);
+
+     }
+  }
+     getch();
+}
+
+
+
+
