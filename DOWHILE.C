1b9b4432bd4ec6daae35e79dba66b209b91df6d7
hastigohil89 <hastigohil89@gmail.com>
2025-07-04 21:26:13 -0700

    Add files via upload

---
+++ DOWHILE.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int n;
+  clrscr();
+  printf("enter N:");
+  scanf("%d",&n);
+  do
+  {
+     printf("\n tops technologies");
+     n--;
+  }while(n<0);
+  getch();
+}
